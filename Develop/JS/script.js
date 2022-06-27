e11efcba7e9b81a9ddf260d96e0e709b84b5662b
JamesB <James.buchmann@gmail.com>
2022-06-27 17:43:35 -0500

    changing the flow of the coding

--- Develop/JS/script.js
+++ Develop/JS/script.js
@@ -4,16 +4,68 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate")
 
-function generatePassword() {
-  var passwordLength = prompt("Choose password length between 8 and 128");
-  var uppercase = confirm("do you want uppercase letters in your password?");
-  var lowercase = confirm("do you want lowercase letters?");
-  var numerics = confirm("Do you want numbers in your password?")
-  var specialCharacters = confirm("do you want special characters in your password?")
-  var passwordResult = '';
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-  console.log(passwordLength, uppercase, lowercase, numerics, specialCharacters, passwordResult);
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+// 32 charaters btw
+var characters = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+"-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"];
+
+function generatePassword() {
+  passLength = prompt("Choose a password length between 8 and 128");
+  // check to make sure user inputs something
+  if (!passLength) {
+    alert("this needs a value");
+  }
+  //checks for correct response
+  else if (passLength < 8 || passLength > 128) {
+    passLength = prompt("please choose a number between 8 and 128");
+  }
+  else {
+  // gathering more information
+    numerics = confirm("do you want numbers in your password?");
+    specialCharacters = confirm("do you want special characters in your password?");
+    lowercase = confirm("do you want to inlcude lowercase letters?");
+    uppercase = confrim("do you want to inclue uppercase letters?");
+  } 
+
+  console.log(uppercase, lowercase, specialCharacters, numerics);
+
 }
+
+
+// "if (upper + lower + numeric + spSymbols === 0) {
+//   alert('You must have at least one character type selected for your password!')
+// }"
+
+
+
+// creating the arrays for the password
+var main = []
+
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+"M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+// 32 charaters
+var characters = ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+"-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"] 
+
+
+
+
+
+
+
+
+
 
 
 // passwordLength =Number(length); // this is how you check if pass length is between 8 - 128
@@ -24,12 +76,7 @@ function generatePassword() {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var passwordLength = passwordLength;
-  var password =""
-  for (var i = 0; i <= passwordLength; i++) {
-    
-  }
+
 
   passwordText.value = password;
 
